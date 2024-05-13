e10e1c8c89079a07506a0cb4aa5d77dbd66a85d6
Bryan <bryanantoniolopez@udetijuana.edu.mx>
2024-05-13 12:32:46 -0700

    Update server

--- functions/server.js
+++ functions/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import serverless from "serverless-http";
-import { getMyTop5PowerRangersSeasons } from "./topSeasons.js";
+import { transportRoutes } from "./getRoutes.js";
 
 var app = express();
 app.use(cors());
@@ -9,11 +9,11 @@ app.use(cors());
 var port = process.env.PORT || 5000;
 const router = express.Router();
 
-router.get("/my-top-5-power-rangers-seasons", function (req, res) {
-  res.json(getMyTop5PowerRangersSeasons());
+router.get("/routes", function (req, res) {
+  res.json(transportRoutes());
 });
 
 // Iniciar servidor
-app.use('/.netlify/functions/server', router);
+app.use("/.netlify/functions/server", router);
 export const handler = serverless(app);
 // app.listen(port);
